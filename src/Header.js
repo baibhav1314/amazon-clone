90f62c83f4b8b3f5800213ed7a58e56f7cc9bb2a
baibhav1314 <baibhav.p1314@gmail.com>
2021-08-08 02:50:08 +0530

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { ShoppingBasket } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <div className="header">
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt="amazon logo"
+                />
+            </Link>
+
+            <div className="header__search">
+                <input
+                    className="header__searchInput"
+                    type="text"
+                    id=""
+                    name=""
+                    placeholder=""
+                />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header_optionLineOne">Hello Guest</span>
+                    <span className="header_optionLineTwo">Sign in</span>
+                </div>
+                <div className="header__option">
+                    <span className="header_optionLineOne">Returns</span>
+                    <span className="header_optionLineTwo">&Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header_optionLineOne">Your</span>
+                    <span className="header_optionLineTwo">Prime</span>
+                </div>
+
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasket />
+                        <span className="header_optionLineTwo header__basketcount">
+                            0
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
